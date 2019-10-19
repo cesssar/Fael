918f6300c3f56815623a00124c30bcf132f07917
Cesar Steinmeier <cesssar@me.com>
2019-10-19 08:02:27 -0300

    Ajustes arquivo config

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+#caminhos absolutos
+$pastaInterna="";
+define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$pastaInterna}");
+(substr($_SERVER['DOCUMENT_ROOT'],-1)=='/')?$barra="":$barra="/";
+define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$barra}{$pastaInterna}");
+
+#atalhos
+define('DIRIMG',DIRPAGE."img/");
+define('DIRCSS',DIRPAGE."lib/css/");
+define('DIRJS',DIRPAGE."lib/js/");
+
+#acesso ao banco de dados
+define('HOST',"localhost");
+define('BD',"sistema");
+define('USER',"root");
+define('PASS',"ozzy");
+?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+include("config/config.php");
+include("lib/vendor/autoload.php");
+use Classes\ClassTeste;
+
+?>
+
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -5,6 +13,9 @@
     <title>Fael - Desenvolvimento Web</title>
 </head>
 <body>
+<img src="<?php echo DIRIMG."logo.png"?>"/>
+<?php
 
+?>
 </body>
 </html>
