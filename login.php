94659c9b4dbe2d7a9aabb0ef9d0aa5c7d124be71
cesssar <cesssar@me.com>
2019-11-05 05:24:12 -0300

    Falta javascript

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
                     <form name="frmLogin" class="w-clearfix" action="valida_usuario.php" method="post">
                     <input type="text" name="usuario" data-name="usuario" placeholder="Digite seu usuário" maxlength="256" required="" id="usuario" class="field w-input"/>
                     <input type="password" name="senha" data-name="senha" placeholder="Digite sua senha" maxlength="256" required="" id="senha" class="field w-input"/>
-                    <input type="button" value="Entrar" class="button w-button" onclick="validaLogin()"/>
+                    <input type="submit" value="Entrar" class="button w-button"/>
                     </form>
                 </div>
                 <?php
